35f487c9fcd97ce22fc516144475ffae27aba26e
muddassir0 <150804932+muddassir0@users.noreply.github.com>
2024-02-24 02:45:34 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull muddasir123/simple-python:tagname
+docker pull muddasir1:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 muddasir123/simple-python:tagname
+docker run -d -p 5000:5000 muddasir123:tagname
