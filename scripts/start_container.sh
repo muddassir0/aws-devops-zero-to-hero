9680f0b3afbb4a02a33617d73b2fc858db8714c5
muddassir0 <150804932+muddassir0@users.noreply.github.com>
2024-02-23 16:53:17 +0530

    updating with my github details

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull muddasir123/simple-python:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 muddasir123/simple-python:tagname
